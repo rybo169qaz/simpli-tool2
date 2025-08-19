30fd4d7eced0dc6b026a22304706d9fd6b8ebd2f
Rob Ryan <account@simpliface.info>
2025-08-19 23:49:54 +0100

    added test for using the force option

--- Deployment-Info/utils/test_create_png_icon.py
+++ Deployment-Info/utils/test_create_png_icon.py
@@ -1,6 +1,7 @@
 import os.path
 import pytest
 import tempfile
+from pathlib import Path
 from create_png_icon import CreatePng
 
 # To invoke:
@@ -57,6 +58,13 @@ class TestCreatePng:
         assert os.path.isfile(fname2) is False
         assert len(os.listdir(tmpdir)) == 0
 
+    def test_file_already_exists(self):
+        with pytest.raises(FileExistsError) as excinfo:
+            filename = create_test_folder() + '/abc.png'
+            Path(filename).touch()
+            obj = CreatePng(filename, 'ABC')
+            obj.generate_image()
+        assert str(excinfo.value) == 'File already exists. Set file_overwrite if want to overwrite.'
 
     def test_good_simple_with_defaults(self):
         tmpdir = create_test_folder()
@@ -65,4 +73,20 @@ class TestCreatePng:
         obj.generate_image()
         assert os.path.isfile(fname) is True
         assert len(os.listdir(tmpdir)) == 1
+
+    def test_overwrite_existing_file(self):
+        tmpdir = create_test_folder()
+        fname = tmpdir + '/pqr.png'
+        Path(fname).touch()
+        touched_file_size = os.path.getsize(fname)
+        print(f"The file size is: {touched_file_size} bytes")
+        assert touched_file_size == 0 # this is just testing the pre-conditions
+
+        obj = CreatePng(fname, 'ABC', file_overwrite=True)
+        obj.generate_image()
+        assert os.path.isfile(fname) is True
+        assert len(os.listdir(tmpdir)) == 1
+        post_method_file_size = os.path.getsize(fname)
+        print(f"The file size is: {post_method_file_size} bytes")
+        assert post_method_file_size != 0
 
